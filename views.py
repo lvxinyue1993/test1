ed6a291e6f31dd044095f4ad18de9ed28d5e1484
simida <2421560914@qq.com>
2020-11-13 20:14:23 +0800

    创建视图

--- views.py
+++ views.py
@@ -1,4 +1,9 @@
 from django.http import HTTPResopnse
+from django.shortcuts import redirect
 
 def index(request):
     return HTTPResponse('index')
+
+def login(request):
+    return redirect('/index')
+
